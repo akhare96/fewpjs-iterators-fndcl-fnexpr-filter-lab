c8f30663e07ab9657ceccd758d100d45dd85a831
Ambar Khare <ambar.khare24@gmail.com>
2022-08-09 09:04:03 -0700

    commit tree b590fc7344680c262bfcdffe9324ae34ba8a097f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(stringMatch => stringMatch.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(stringMatch => stringMatch.toLowerCase().indexOf(string.toLowerCase()) === true)
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(stringMatch => stringMatch.name === string)
+}
